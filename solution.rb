4a834900d297cd23a2d90e9a0b20843711181cf5
Oblum1989 <oscar.blum@gmail.com>
2019-03-14 17:59:27 -0500

    commit inicial

---
+++ solution.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+get '/' do
+    unless params[:nombre] 
+        "Hola Desconocido!"
+    else 
+        if params[:nombre] == ""
+            "Hola Desconocido!"
+        else
+            <<-HTML
+                <h1>"Hola #{params[:nombre]}!"</h1>
+            HTML
+        end
+    end
+    
+end
