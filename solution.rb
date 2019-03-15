c0f6ee8d1615add2626a4182571ef98ad997dde0
Oblum1989 <oscar.blum@gmail.com>
2019-03-14 23:32:46 -0500

    solucion  al reto 3.1

--- solution.rb
+++ solution.rb
@@ -4,7 +4,7 @@ get '/' do
     <<-HTML
         <form action="/views/saludo" method="post">
             <input type="text" name="nombre" id="nombre">
-            <button type="submit">Hola</button>
+            <input type="submit" value="Hola!">
         </form>
     HTML
 end
