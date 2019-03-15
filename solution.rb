f015a1b8cca2462ca75acd3e8f6f88b39e05129a
Oblum1989 <oscar.blum@gmail.com>
2019-03-15 00:06:27 -0500

    solucion  al reto 4

--- solution.rb
+++ solution.rb
@@ -1,14 +1,19 @@
 require 'sinatra'
+###### reto 3
+# get '/' do
+#     <<-HTML
+#         <form action="/views/saludo" method="post">
+#             <input type="text" name="nombre" id="nombre">
+#             <input type="submit" value="Hola!">
+#         </form>
+#     HTML
+# end
 
+# post '/views/saludo' do
+#     "Hola #{params[:nombre]}"
+# end
+ ###################reto 4
+ 
 get '/' do
-    <<-HTML
-        <form action="/views/saludo" method="post">
-            <input type="text" name="nombre" id="nombre">
-            <input type="submit" value="Hola!">
-        </form>
-    HTML
-end
-
-post '/views/saludo' do
-    "Hola #{params[:nombre]}"
+   erb :index
 end
